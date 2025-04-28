ae90e832fb0bbe071c91ea01d82f325dea48a4a2
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-04-28 15:31:59 +0200

    [Modify] modifications mineures

---
+++ TeleCheminee/src/MainAppartement.java
@@ -0,0 +1,11 @@
+public class MainAppartement {
+
+    public static void main(String [] args) {
+
+        AppartementAdapter apadp = new AppartementAdapter();
+        apadp.chargerTelecommande();
+        TelecommandeGraphique teleGraph = new TelecommandeGraphique();
+
+    }
+
+}
